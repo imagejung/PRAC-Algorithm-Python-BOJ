8e542ab0696c470b699115eccb16f519b5d4f0d9
imagejung <ysjung9312@gmail.com>
2023-06-14 14:33:43 +0900

    230614

--- PAI/PAI_01.py
+++ PAI/PAI_01.py
@@ -1,2 +1,16 @@
 # 유요한 팰린트롬, p138
 
+class Solution(object):
+    # 문자 or 숫자만 받고, 전부 소문자로 배열에 추가
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+        for char in s:
+            if char.isalnum(): # 문자 or 숫자인 경우만 strs 배열에 추가
+                strs.append(char.lower())
+
+        # 팰린트롬인지 확인
+        while len(strs) > 1:
+            if strs.pop(0) != strs.pop():
+                return False
+
+        return True
