cabbb3f24e8d74ddff7aba4d5c3310573ce5b81f
imagejung <ysjung9312@gmail.com>
2023-06-19 21:33:49 +0900

    230619, 문자열조작

--- PAI/PAI_03.py
+++ PAI/PAI_03.py
@@ -1,9 +1,18 @@
-# 로그파일 재정렬, p000
-# test
+# 로그파일 재정렬, p148
 
 class Solution:
     def reorderLogFiles(self, logs: list[str]) -> list[str]:
+        letters, digits = [],[]
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
 
+        # 람다 표현식으로 정렬
+        letters.sort(key=lambda x : (x.split()[1:], x.split()[0]))
+
+        return letters + digits
 
 
 
