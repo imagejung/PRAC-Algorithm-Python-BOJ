ef214039a993f4b36863bb0d8ed1d661f046189d
imagejung <ysjung9312@gmail.com>
2023-07-03 23:06:02 +0900

    230703, 배열

--- PAI/PAI_10.py
+++ PAI/PAI_10.py
@@ -1,2 +1,15 @@
 # 배열 파티션1, p190
 
+class Solution:
+    def arrayPairSum(self, nums: list[int]) -> int:
+        sum = 0
+        nums.sort()
+        tmp = [] # sorting해서 앞에서 부터 2개씩 집어넣으면 답 나옴
+
+        for n in nums:
+            tmp.append(n)
+            if len(tmp) == 2:
+                sum += min(tmp)
+                tmp = [] # 2개 집어 넣었으니까, tmp 초기화
+
+        return sum
