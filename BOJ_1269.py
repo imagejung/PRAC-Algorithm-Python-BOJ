af72f9a5917583f0c1bb2d56f0a7b3d0adabfbac
imagejung <ysjung9312@gmail.com>
2023-07-10 01:30:30 +0900

    230710, 자료구조, set자료형

--- BOJ_1269.py
+++ BOJ_1269.py
@@ -1,3 +1,15 @@
 # 대칭 차집합, 1269
 
+import sys
+input = sys.stdin.readline
+
+num_a, num_b = map(int, input().split())
+a = set(map(int, input().split()))
+b = set(map(int, input().split()))
+
+# set으로 하면 뺄 수 있음
+print(len(a-b) + len(b-a))
+
+
+
 
